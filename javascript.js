26448476205dae84e7bb169a41497fc16b769a4d
Ved <vishalv9601@gmail.com>
2024-07-24 15:46:36 +0530

    Adding project files

---
+++ javascript.js
@@ -0,0 +1,59 @@
+// 
+let timer = document.querySelector(".timer");
+let hr = 0;
+let min = 0;
+let sec = 0;
+let stoptime = true;
+
+function startTimer() {
+  if (stoptime == true) {
+    stoptime = false;
+    timerCycle();
+  }
+}
+function pauseTimer() {
+  if (stoptime == false) {
+    stoptime = true;
+  }
+}
+
+function timerCycle() {
+  if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    hr = parseInt(hr);
+
+    sec = sec + 1;
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
+
+    if (sec < 10 ) {
+      sec = "0" + sec;
+    }
+    if (min < 10 ) {
+      min = "0" + min;
+    }
+    if (hr < 10 ) {
+      hr = "0" + hr;
+    }
+
+    timer.innerText = hr + ":" + min + ":" + sec;
+
+    setTimeout("timerCycle()", 1000);
+  }
+}
+function resetTimer() {
+  timer.innerHTML = "00:00:00";
+  stoptime = true;
+  hr = 0;
+  sec = 0;
+  min = 0;
+}
